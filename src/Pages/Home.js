6f00cf426fecaada0c39182a54bc908cc55a973a
Joao Pedro <joao.ricardo88@gmail.com>
2020-10-24 00:38:52 +0100

    added circle

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as GoIcons  from 'react-icons/go';
 
 function Home() {
+   
+    const [size, setSize] = useState (750);
+    const reduceSize = () => setSize(size - 20); 
+  
     return (
         <div className='home'>
-            <h1>Home Page</h1>
+            <h1>Teste</h1>
+            
+            <GoIcons.GoPrimitiveDot
+            size={size}
+            color="red"
+            />
+            
+            <button className="buttonSize" onClick={reduceSize}>Size</button>
       </div>
     )
 }
